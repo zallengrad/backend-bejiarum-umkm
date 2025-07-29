097d61f05a74e22fef55fb674faad4ebc7863e29
Muhammad Afrizale <141467689+zallengrad@users.noreply.github.com>
2025-07-29 12:18:11 +0700

    merevisi backend - menghilangkan visualisasi peta

--- routes/mapsRoutes.js
+++
@@ -1,8 +0,0 @@
-// backend/routes/mapsRoutes.js
-const express = require("express");
-const router = express.Router();
-const { getLatLngFromMapsUrl } = require("../controllers/mapsController");
-
-router.get("/resolve", getLatLngFromMapsUrl);
-
-module.exports = router;
